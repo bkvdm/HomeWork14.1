4d2a73a5ebfcb7183fe10070a4d2a354447c56f6
bkvdm <vadim@991199.ru>
2023-09-08 18:10:44 +0300

    Домашняя работа №14.1 ## Задание 5
    Напишите метод, который изменяет значение переменной типа int. Объявите метод changeValue, который принимает параметр типа int под названием value. Внутри метода с помощью оператора присваивания запишите в параметр значение 22.
    
    В методе main объявите переменную типа int под названием value и запишите туда значение 33. В следующей строке вызовите метод changeValue и передайте в него эту переменную.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 public class Main {
-    public static int changeValue(int value) {
-        int changValue = 22;
-        return changValue;
+    public static void changeValue(int value) {
+        value = 22;
+        System.out.println("value = " + value);
     }
 
     public static void main (String[] arg) {
         int value = 33;
-        value = changeValue(value);
-        System.out.println("value = " + value);
+        changeValue(value);
     }
 }
+
+//    Напишите метод, который изменяет значение переменной типа int.
+//    Объявите метод changeValue, который принимает параметр типа int под названием value.
+//    Внутри метода с помощью оператора присваивания запишите в параметр значение 22.
+//     В методе main объявите переменную типа int под названием value и запишите туда значение 33.
+//     В следующей строке вызовите метод changeValue и передайте в него эту переменную.
